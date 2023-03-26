4ffde9b27bca576bd03ee125bc5595133a97e434
lubentancor <bentancorlucia@gmail.com>
2023-03-26 19:40:31 -0300

    navbar con efecto

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 //carousel
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+
+        $(".carousel").owlCarousel({
+            margin: 10,
+            loop:true,
+            autoplay:true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            responsive: {
+              0:{
+                items: 1,
+                nav: false
+              },
+              600:{
+                items: 2,
+                nav: false
+              },
+              1000:{
+                items: 3,
+                nav: false
+              } 
+            }
+
+        });
+
+
+// scroll 
+
+const headerEl = document.querySelector('.header');
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+            headerEl.classList.add('header-scrolled')
+            } else if (window.scrollY <=50) {
+                headerEl.classList.remove('header-scrolled') 
+            }
+        })
