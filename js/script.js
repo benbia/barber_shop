a0024d04655f842e6b9fbd3411e7dd2c5c12938a
lubentancor <bentancorlucia@gmail.com>
2023-03-26 22:10:42 -0300

    landscape y eso

--- js/script.js
+++ js/script.js
@@ -3,21 +3,19 @@
         $(".carousel").owlCarousel({
             margin: 10,
             loop:true,
+            nav: false,
             autoplay:true,
             autoplayTimeout: 2000,
             autoplayHoverPause: true,
             responsive: {
               0:{
                 items: 1,
-                nav: false
               },
               600:{
                 items: 2,
-                nav: false
               },
               1000:{
                 items: 3,
-                nav: false
               } 
             }
 
